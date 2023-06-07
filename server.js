26b2a7e8e8bf745313061331efd89e41331dff16
Ronard Nyongkah <ronardnyongkah@Ronards-MacBook-Air.local>
2023-06-07 16:10:37 -0500

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -29,6 +29,18 @@ app.get("/tip/:total/:tipPercentage", (req,res) =>{
     res.send((all.toString()))
 })
 
+app.get('/magic/:wish', (req,res)=>{
+    // Testing
+    console.log(req.params)
+    
+    const wish = req.params.wish
+    
+    // list of responses
+    const options = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    let randumNum = Math.floor(Math.random() * options.length)
+    res.send(`${wish} <h1>${options[randumNum]}</h1>`)
+
+})
 ///////////////////////////
 // Server Listener
 ///////////////////////////
