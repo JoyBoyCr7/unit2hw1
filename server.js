2c5e5da55dc9da3d1dcdea71e0e41fbf1647921f
Ronard Nyongkah <ronardnyongkah@Ronards-MacBook-Air.local>
2023-06-07 13:16:27 -0500

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -15,10 +15,19 @@ app.get("/greeting/",(req,res)=>{
 
 app.get(`/greeting/:name`,(req,res)=>{
     const name = req.params.name
+
     res.send(` Wow! Hello there, ${name}`)
 })
 
-
+app.get("/tip/:total/:tipPercentage", (req,res) =>{
+    const total = req.params.total 
+    tipPercent = req.params.tipPercentage
+    all = Number(total) * Number(tipPercent) / 100  
+    // testing
+    console.log(all)
+    // testing
+    res.send((all.toString()))
+})
 
 ///////////////////////////
 // Server Listener
